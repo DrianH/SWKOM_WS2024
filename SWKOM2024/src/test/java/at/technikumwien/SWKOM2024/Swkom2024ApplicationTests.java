261553edd2ff348e72d4b733dedaf3c9cf545316
DrianH <drianhoxha02@gmail.com>
2024-09-23 09:14:20 +0200

    Initial Commit - Hardcoded Endpoints - Dockerfile + Docker Compose

---
+++ SWKOM2024/src/test/java/at/technikumwien/SWKOM2024/Swkom2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.SWKOM2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swkom2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
