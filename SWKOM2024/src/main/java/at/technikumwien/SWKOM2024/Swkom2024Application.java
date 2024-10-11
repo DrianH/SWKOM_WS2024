3ca7403245a131252ba69606e8d751cc0f90b4d3
DrianH <drianhoxha02@gmail.com>
2024-10-11 10:14:11 +0200

    Entity + Repo | Restored API Access
    Just needed to run mvn clean package lmfao

--- SWKOM2024/src/main/java/at/technikumwien/SWKOM2024/Swkom2024Application.java
+++ SWKOM2024/src/main/java/at/technikumwien/SWKOM2024/Swkom2024Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"controllers", "entities", "repositories"})
+@SpringBootApplication
 public class Swkom2024Application {
 
 	public static void main(String[] args) {
